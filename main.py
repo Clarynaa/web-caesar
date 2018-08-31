a870df9d6d3e785421c2d3f7f949e2f30667c120
Clarynaa <32001815+Clarynaa@users.noreply.github.com>
2018-08-30 20:12:42 -0500

    finished

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ form = """<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form action="/" method='POST' name="hi">
             <label for="rot">
                 Rotate by:<input type="text" id="rot" name="rot" value="0"/><br>
-                <input type="textarea" id="text" name="text"/><br>
+                <input type="textarea" id="text" name="text" value="{}"/><br>
                 <input type="submit"/>
             </label>
         </form>
@@ -38,17 +38,13 @@ form = """<!DOCTYPE html>
 
 @app.route("/")
 def index():
-    content = form
-    return content
-
-@app.route("/caesar", methods=['POST'])
-def caesar():
-    return request.form['text']
+    return form.format("")
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = request.form['text']
-    return "<h1>{}</h1>".format(rotate_string(text, rot))
+    return form.format(rotate_string(text, rot))
+    #return "<h1>{}</h1>".format(rotate_string(text, rot))
 
 app.run()
